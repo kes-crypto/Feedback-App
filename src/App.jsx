87be1a338f9f4333ec6c638a07af857d9ec358be
Dennis Kestone <denniskestone@gmail.com>
2024-01-23 22:29:39 +0300

    Components Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,35 @@
-import { Suggestion, Navbar, Sidebar } from "./Components";
 import "./index.css";
+import Logo from "./Components/Sidebar/Logo";
+import Categories from "./Components/Sidebar/Categories";
+import Suggestions from "./Components/Suggestion";
+import Menu from "./Components/Navbar";
 
 function App() {
   return (
     <>
-      <div class="">
-        <Sidebar />
+      <div className="nav-bar md:flex lg:flex-col">
+        <div className="lg:flex">
+          <div className="md:flex lg:flex-col">
+            <Logo />
+            <div className="md:flex lg:flex">
+              <Categories />
+            </div>
+          </div>
+          <div className="">
+            <Menu />
+            <Suggestions />
+          </div>
+        </div>
+        <div className="md:hidden lg:hidden">
+          <Categories />
+        </div>
       </div>
-      <div>
-        <Navbar />
-        <Suggestion />
+
+      <div className=" hidden md:hidden">
+        <Menu />
+      </div>
+      <div className=" hidden md:hidden">
+        <Suggestions />
       </div>
     </>
   );
